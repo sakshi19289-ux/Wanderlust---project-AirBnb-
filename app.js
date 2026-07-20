2924b4126c9d8c656d90f1aa3a957a68600caabc
Sakshi Bhatt <sakshi19289@gmail.com>
2026-07-21 01:11:11 +0530

    Initial project setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require ("express");
+const app = express();
+const mongoose = require("mongoose")
+
+
+app.listen("8080",()=>{
+    console.log("server is listening on port 8080 ")
+})
